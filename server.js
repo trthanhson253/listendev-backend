84effc5889e118457bd3cdd20d37a21967ab9a35
trthanhson253 <trthanhson253@gmail.com>
2020-06-16 09:54:09 -0400

    second

--- server.js
+++ server.js
@@ -32,7 +32,19 @@ app.use(cookieParser());
 
 // cors
 if (process.env.NODE_ENV === 'development') {
-  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+  // app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+  });
 }
 // routes middleware
 app.use('/api', companyRoutes);
